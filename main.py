ef0e35a0fb77a3f0e9b4848508ad811c0bedcbf1
alecdotico <altapas@gmail.com>
2014-06-04 15:24:49 +0200

    step2 - post comments and likes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication, template
 from os.path import join, dirname
-from models import Post
+from models import Post, ndb, Comment
 
 
 class MainHandler(RequestHandler):
@@ -12,13 +12,13 @@ class MainHandler(RequestHandler):
 
 class HomeHandler(RequestHandler):
     def get(self):
-        posts = Post.query().order(-Post.date).fetch(10)
+        posts = Post.query().order(-Post.date).fetch()
         logging.info("HomeHandler - sirviendo {} posts".format(len(posts)))
         html = render('home.html', {'posts': posts})
         self.response.write(html)
 
 
-class PostCreate(RequestHandler):
+class PostCreateHandler(RequestHandler):
     def get(self):
         html = render('post_create.html')
         self.response.write(html)
@@ -32,10 +32,34 @@ class PostCreate(RequestHandler):
         self.redirect('/')
 
 
+class PostHandler(RequestHandler):
+    def get(self, post_id):
+        post = Post.get_by_id(int(post_id))
+        html = render('post.html', {'post': post})
+        self.response.out.write(html)
+
+    def post(self, post_id):
+        text = self.request.get('text', '')
+        if len(text):
+            Comment(text=text, post=ndb.Key(Post, int(post_id))).put()
+        self.redirect('/post/' + post_id)
+
+
+class LikePostHandler(RequestHandler):
+    def get(self, post_id):
+        post = Post.get_by_id(int(post_id))
+        if post:
+            post.likes += 1
+            post.put()
+        self.redirect(self.request.referer)
+
+
 app = WSGIApplication([
     ('/hello', MainHandler),
     ('/', HomeHandler),
-    ('/post/create', PostCreate)
+    ('/post/create', PostCreateHandler),
+    (r'/post/(?P<post_id>\d+)', PostHandler),
+    (r'/post/(?P<post_id>\d+)/like', LikePostHandler),
 ], debug=True)
 
 
--- models.py
+++ models.py
@@ -4,8 +4,20 @@ from google.appengine.ext import ndb
 class Post(ndb.Model):
     title = ndb.StringProperty()
     media_url = ndb.StringProperty()
-    likes = ndb.IntegerProperty()
+    likes = ndb.IntegerProperty(default=0)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+    def get_comments(self):
+        return Comment.query(Comment.post == self.key)
+
+    def comment_count(self):
+        return self.get_comments().count()
+
+    def get_absolute_url(self):
+        return "/post/" + str(self.key.id())
 
 
+class Comment(ndb.Model):
+    post = ndb.KeyProperty()
+    text = ndb.TextProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
